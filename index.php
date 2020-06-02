74f5505d63999e1b191d37f1482500fedfd7f7a2
ejanssen <ejanssen@ewe.net>
2020-06-02 02:14:23 +0200

    footer.php angelegt und gegen fußzeile in jeder datei getauscht

--- index.php
+++ index.php
@@ -72,16 +72,9 @@
         </section>
     </div>
 
-    <footer>
-        <hr>
-        <nav>
-            <p class="footer">
-                <a href="impragb.html" class="btn btn-link">AGB - Impressum - Datenschutz</a> |
-                <a href="about.html" class="btn btn-link">Über uns</a> |
-                <a href="login.html" class="btn btn-link">(Anmelden / Abmelden?)</a>
-            </p>
-        </nav>
-    </footer>
+    <?php
+        include "php/footer.php";
+    ?>
 </body>
 
 </html>
