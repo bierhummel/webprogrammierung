af140cfc675e9ef5d66b951045131620b08c0ca6
ejanssen <ejanssen@ewe.net>
2020-06-03 16:56:34 +0200

    Alte auskommentierte Version von about rausgeworfen

--- about.php
+++ about.php
@@ -8,8 +8,7 @@ session_start();
 ?>
 
 <!DOCTYPE HTML>
-<html lang="de">
-
+<html lang="de">  
     
 <head>
     <meta charset="utf-8">
@@ -30,60 +29,11 @@ session_start();
         $title = "OSJB";
         include "php/header.php";
     ?>
-    <!-- <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="bilder/3.jpg" class="d-block w-100 img-fluid mx-auto d-block rounded" alt="slide 1">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5> Herzlich Willkommen auf unserer Website!</h5>
-                    <p>Unser Ziel ist es, Studierenden und Absolventen dabei zu helfen,
-                        den perfekten Job zu finden! Darum haben wir uns dazu entschlossen, eine Platform zu gründen die auf die
-                        Bedürfnisse und Qualifikationen von Akademikern zugeschnitten ist. Egal ob Praktikum, Werkstudenten- oder
-                        Minijob; bei uns findest du eine breite Auswahl von Angeboten in deiner Nähe die auch mit deinem
-                        Studienalltag/Stundenplan vereinbar sind! Uns liegen die lokale Unternehmen und Start Ups am Herzen, deswegen
-                        möchten wir den Bewerbngsablauf so einfach und unbürokratisch wie möglich gestalten.</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="bilder/2.jpg" class="d-block w-100" alt="slide 2">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Wie funktioniert's?</h5>
-                    <p>Gib einfach auf unserer Startseite deine Postleitzahl, den gewünschten Umkreis und deine Fakultät an, wo du
-                        sudierst oder deinen Abschluss gemacht hast. Über die Filterfunktionen kannst du dann die Beschäftigungsart,
-                        Zeitintensität, etc auswählen. Über den Button "bewerben!" gelangst du direkt auf die Seite des Unternehmens
-                        wo du deine Unterlagen einreichen kannst. <br>
-                        <br>
-                        Für dich als Arbeitnehmer ist somit keine Registrierung erforderlich!</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="bilder/1.jpg" class="d-block w-100" alt="slide 3">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5> Für Arbeitgeber</h5>
-                    <p> Sofern Sie selbst eine Jobanzeige aufgeben möchten, bitten wir Sie sich zu registrieren und ein Profil zu hinterlegen.</p>
-                </div>
-            </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>-->
-
+    
     <div class="row">
         <div class="container col-sm-3">
             <section>
                 <h2>
-                    <!--  Herzlich Willkommen auf unserer Website!-->
                     Unser Ziel
                 </h2>
                 <p>
@@ -137,4 +87,5 @@ session_start();
         include "php/footer.php";
     ?>
 
-</body></html>
+</body>
+</html>
